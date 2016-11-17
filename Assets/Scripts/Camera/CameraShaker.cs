5ec6f21b9acf6bd4cc6366658ebf597cbc0fa9f3
Pavel Smejkal <smejkal.pa@gmail.com>
2016-11-17 11:27:18 +0100

    Refactor CameraShaker

--- Assets/Scripts/Camera/CameraShaker.cs
+++ Assets/Scripts/Camera/CameraShaker.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraShaker
 {
@@ -11,6 +9,12 @@ public class CameraShaker
     private float speed;
     private float elapsed;
 
+    /// <summary>
+    /// Set parameters for shake. When NextShake is called offsets will be based on these.
+    /// </summary>
+    /// <param name="duration"></param>
+    /// <param name="magnitude"></param>
+    /// <param name="speed"></param>
     public void InitNewShake(float duration, float magnitude, float speed)
     {
         this.duration = duration;
@@ -24,6 +28,11 @@ public class CameraShaker
         duration = 0f;
     }
 
+    /// <summary>
+    /// Should be called from Update method.
+    /// Returns X, Y offsets for the next shake
+    /// </summary>
+    /// <returns></returns>
     public Vector3 NextShake()
     {
         if (elapsed < duration)
