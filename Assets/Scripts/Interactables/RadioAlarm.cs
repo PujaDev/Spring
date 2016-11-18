fa50bcf22496b6531d1071b09621b3d69c48d71e
Ursula Zakovska <u.zakovska@gmail.com>
2016-11-18 16:24:38 +0100

    Added interactable object to scene 1 and set sorting layers hierarchy

---
+++ Assets/Scripts/Interactables/RadioAlarm.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RadioAlarm : IInteractable {
+    protected override Action[] getActionList()
+    {
+        return new Action[] {
+            new Action(ActionType.TURN_ALARM_OFF, "Turn alarm off",icons[0]),
+            new Action(ActionType.POSTPONE_ALARM, "Postpone alarm",icons[1]),
+        };
+    }
+    
+}
