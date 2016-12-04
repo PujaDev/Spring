ea6684b564d65d105744940d32125591620bb655
Pavel Smejkal <smejkal.pa@gmail.com>
2016-12-05 00:33:23 +0100

    Add Switches and position generaton to separate scene

---
+++ Assets/Scripts/LockPuzzle/LockController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LockController : MonoBehaviour
+{
+    public int SwitchCount;
+    public int SwitchOffset;
+    public int CorrectAnswer;
+    public BoxCollider2D ButtonArea;
+
+    public bool IsCorrect { get { return CorrectAnswer == CurrentAnswer; } }
+
+    private int CurrentAnswer;
+
+    void Awake()
+    {
+        int value = 1;
+        var positions = GenerateSwitchPositions();
+        for (int i = 0; i < SwitchCount; i++)
+        {
+            GameObject s = Switch.Create(value, ClickCallback, positions[i]);
+            s.transform.parent = transform;
+
+            value <<= 1;
+        }
+    }
+
+    void ClickCallback(bool isOn, int value)
+    {
+        if(isOn)
+        {
+            CurrentAnswer |= value;
+        }
+        else
+        {
+            CurrentAnswer &= ~value;
+        }
+        Debug.Log(CurrentAnswer);
+    }
+
+    private List<Vector3> GenerateSwitchPositions()
+    {
+        List<Vector3> positions = new List<Vector3>();
+
+        float offset = ButtonArea.bounds.size.x / (SwitchCount - 1);
+
+        for (int i = 0; i < SwitchCount; i++)
+        {
+            positions.Add(new Vector3(ButtonArea.bounds.min.x + i * offset, ButtonArea.bounds.center.y, ButtonArea.bounds.center.z));
+        }
+
+        return positions;
+    }
+}
---
+++ Assets/Scripts/LockPuzzle/Switch.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Switch : MonoBehaviour
+{
+    private bool IsOn;
+    private int Value;
+    private Action<bool, int> OnClickCallback;
+
+    void OnMouseDown()
+    {
+        IsOn = !IsOn;
+        var r = GetComponent<SpriteRenderer>();
+        if(IsOn)
+        {
+            r.color = Color.red;
+        }
+        else
+        {
+            r.color = Color.white;
+        }
+
+        OnClickCallback(IsOn, Value);
+    }
+
+
+    #region Factory
+    static bool Loaded;
+    static UnityEngine.Object prefab;
+
+    public static GameObject Create(int value, Action<bool, int> onClickCallback, Vector3 position)
+    {
+        if (!Loaded)
+        {
+            prefab = Resources.Load("Prefabs/LockPuzzle/Switch");
+            Loaded = true;
+        }
+
+        GameObject swch = Instantiate(prefab) as GameObject;
+        var s = swch.GetComponent<Switch>();
+
+        s.Value = value;
+        s.OnClickCallback = onClickCallback;
+        swch.transform.position = position;
+
+        return swch;
+    }
+    #endregion
+}
