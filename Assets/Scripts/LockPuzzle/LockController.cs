d2b1c4200f3644f202de202de52e9f3be05f4bc0
Pavel Smejkal <smejkal.pa@gmail.com>
2016-12-05 14:51:07 +0100

    Implement working column functionality

--- Assets/Scripts/LockPuzzle/LockController.cs
+++ Assets/Scripts/LockPuzzle/LockController.cs
@@ -8,22 +8,47 @@ public class LockController : MonoBehaviour
     public int SwitchOffset;
     public int CorrectAnswer;
     public BoxCollider2D ButtonArea;
+    public BoxCollider2D ColumnArea;
+    public GameObject StepPrefab;
+    public GameObject PointerPrefab;
 
     public bool IsCorrect { get { return CorrectAnswer == CurrentAnswer; } }
 
     private int CurrentAnswer;
+    private GameObject Pointer;
+    private List<Vector3> StepPositions;
+    private Vector3 PtrShift;
+    private Coroutine PtrMovement;
 
     void Awake()
     {
         int value = 1;
-        var positions = GenerateSwitchPositions();
+        var switchPositions = GeneratePositions(ButtonArea, SwitchCount, true);
         for (int i = 0; i < SwitchCount; i++)
         {
-            GameObject s = Switch.Create(value, ClickCallback, positions[i]);
+            GameObject s = Switch.Create(value, ClickCallback, switchPositions[i]);
             s.transform.parent = transform;
 
             value <<= 1;
         }
+
+        int stepCount = 1 << SwitchCount;
+        StepPositions = GeneratePositions(ColumnArea, stepCount, false);
+        for (int i = 0; i < stepCount; i++)
+        {
+            GameObject s = Instantiate(StepPrefab, StepPositions[i], Quaternion.identity) as GameObject;
+            s.transform.parent = transform;
+            var rend = s.GetComponent<SpriteRenderer>();
+
+            if (i == CorrectAnswer)
+                rend.color = Color.green;
+            else if (i == 0)
+                rend.color = Color.black;
+            else
+                rend.color = Color.yellow;
+        }
+        PtrShift = new Vector3(0.35f, 0);
+        Pointer = Instantiate(PointerPrefab, StepPositions[0] + PtrShift, Quaternion.identity) as GameObject;
     }
 
     void ClickCallback(bool isOn, int value)
@@ -37,19 +62,53 @@ public class LockController : MonoBehaviour
             CurrentAnswer &= ~value;
         }
         Debug.Log(CurrentAnswer);
+        MovePointer(CurrentAnswer);
     }
 
-    private List<Vector3> GenerateSwitchPositions()
+    private void MovePointer(int index)
     {
+        if (PtrMovement != null)
+            StopCoroutine(PtrMovement);
+
+        PtrMovement = StartCoroutine(MovePointerCoroutine(StepPositions[index] + PtrShift));
+    }
+
+    IEnumerator MovePointerCoroutine(Vector3 target)
+    {
+        while(Vector3.Distance(Pointer.transform.position, target) > 0.005f)
+        {
+            Pointer.transform.position = Vector3.MoveTowards(Pointer.transform.position, target, 0.05f);
+            yield return null;
+        }
+    }
+
+    private List<Vector3> GeneratePositions(BoxCollider2D area, int count, bool horizontal)
+    {
+        area.enabled = true;
         List<Vector3> positions = new List<Vector3>();
 
-        float offset = ButtonArea.bounds.size.x / (SwitchCount - 1);
+        float offset;
+        if (horizontal)
+            offset = area.bounds.size.x / (count - 1);
+        else
+            offset = area.bounds.size.y / (count - 1);
 
-        for (int i = 0; i < SwitchCount; i++)
+        if (horizontal)
         {
-            positions.Add(new Vector3(ButtonArea.bounds.min.x + i * offset, ButtonArea.bounds.center.y, ButtonArea.bounds.center.z));
+            for (int i = 0; i < count; i++)
+            {
+                positions.Add(new Vector3(area.bounds.min.x + i * offset, area.bounds.center.y, area.bounds.center.z));
+            }
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                positions.Add(new Vector3(area.bounds.center.x, area.bounds.min.y + i * offset, area.bounds.center.z));
+            }
         }
 
+        area.enabled = false;
         return positions;
     }
 }
