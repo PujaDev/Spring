5fe98cc2c55e312ed4ecf141d690ba8e75a13378
Ursula Zakovska <u.zakovska@gmail.com>
2016-12-11 19:30:05 +0100

    Add AnnanasOwl flying

---
+++ Assets/Scripts/SpineAnimatedGameObjects/AnnanasOwlAnimator.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using Fungus;
+
+public class AnnanasOwlAnimator : MonoBehaviour {
+    private SkeletonAnimation skeletonAnim;
+    //public float SpeedFly;
+    //public AnimationCurve Curve;
+
+    private Coroutine idleHead;
+    private Coroutine idleWings;
+
+    //Debug 
+    public bool fly = false;
+    public Transform[] path;
+    public float percentsPerSecond = 0.05f; // %2 of the path moved per second
+    public float currentPathPercent = 0.0f; //min 0, max 1
+    public float scaleParam;
+
+
+    IEnumerator IdleHeadCoroutine() {
+        float randomDelay;
+        while (true)
+        {
+            randomDelay = Random.Range(2f, 5f);
+            yield return new WaitForSeconds(randomDelay);
+            skeletonAnim.AnimationState.AddAnimation(1, "idle_head", false, 0);
+        }
+    }
+
+    IEnumerator IdleWingsCoroutine()
+    {
+        float randomDelay;
+        while (true)
+        {
+            randomDelay = Random.Range(4f, 8f);
+            yield return new WaitForSeconds(randomDelay);
+            skeletonAnim.AnimationState.SetAnimation(0, "idle_wings", false);
+            skeletonAnim.AnimationState.AddAnimation(0, "idle_breathing", true, 0);
+        }
+    }
+
+    IEnumerator FlyToCoroutine()
+    {
+        if (idleHead != null)
+            StopCoroutine(idleHead);
+        if (idleWings != null)
+            StopCoroutine(idleWings);
+
+        skeletonAnim.AnimationState.SetAnimation(0, "start_flight", false);
+        skeletonAnim.AnimationState.AddAnimation(0, "flying", true, 0).timeScale = 4f;
+
+        Spine.ExposedList<Spine.Animation> allAnim = skeletonAnim.skeleton.data.animations;
+        foreach (var item in allAnim) {
+            if (item.name == "start_flight") {
+                yield return new WaitForSeconds(item.duration);
+                break;
+            }
+        }
+               
+        while (Vector3.Distance(transform.position, path[path.Length - 1].position) > 0.05f)
+        {
+            currentPathPercent += percentsPerSecond * Time.deltaTime;
+            iTween.PutOnPath(gameObject, path, currentPathPercent);
+            float tmp = transform.localScale.x - (scaleParam * Time.deltaTime);
+            transform.localScale = new Vector3(tmp, tmp, tmp);
+
+            yield return null;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        skeletonAnim = GetComponent<SkeletonAnimation>();
+        skeletonAnim.AnimationState.SetAnimation(0, "idle_breathing", true);
+        idleHead = StartCoroutine(IdleHeadCoroutine());
+        idleWings = StartCoroutine(IdleWingsCoroutine());
+    }
+
+    void Update() {
+        if (fly) {
+            fly = false;
+            StartCoroutine(FlyToCoroutine());
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpineAnimatedGameObjects/SkyAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+
+public class SkyAnimator : MonoBehaviour {
+    private SkeletonAnimation skeletonAnim;
+
+    // Use this for initialization
+    void Start()
+    {
+        skeletonAnim = GetComponent<SkeletonAnimation>();
+        skeletonAnim.AnimationState.SetAnimation(0, "clouds", true);
+        skeletonAnim.AnimationState.SetAnimation(1, "manta_1", true);
+        skeletonAnim.AnimationState.SetAnimation(2, "manta_2", true);
+        skeletonAnim.AnimationState.SetAnimation(3, "manta_3", true);
+    }
+}
