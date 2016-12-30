6eb1d7b0fa316f3705cfa6e5681446729982b03d
Pavel Smejkal <smejkal.pa@gmail.com>
2016-12-30 18:41:13 +0100

    Add ItemHolder with ability to hold items from inventory

--- Assets/Scripts/UI/Inventory/ItemData.cs
+++ Assets/Scripts/UI/Inventory/ItemData.cs
@@ -6,40 +6,21 @@ using System;
 
 public class ItemData : MonoBehaviour, IPointerClickHandler
 {
-
-    private bool IsDragged = false;
+    private bool IsDragged;
 
     public int DataIndex;
 
-    public void SetItemData(Sprite image, int index) {
+    private Sprite Sprite { get { return gameObject.GetComponent<Image>().sprite; } }
+
+    public void SetItemData(Sprite image, int index)
+    {
         gameObject.GetComponent<Image>().sprite = image;
         DataIndex = index;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left && !IsDragged)
-        {
-            //IsDragged = true;
-            //OriginalPosition = transform.position;
-            //OriginalParent = transform.parent;
-
-            //ItemHolder.Instance.GetComponent<ItemHolder>().StartHolding(Item);
-            //transform.SetParent(ItemHolder.Instance.transform);
-
-            //Inventory.Instance.Close();
-        }
-        else if (eventData.button == PointerEventData.InputButton.Right)
-        {
-            //ReturnToInventory();
-        }
-    }
-
-    public void ReturnToInventory()
-    {
-        //IsDragged = false;
-        //transform.SetParent(OriginalParent);
-        //transform.position = OriginalPosition;
-        //SantaController.controller.changeClothes(Item.Name, true);
+        ItemHolder.Instance.GetComponent<ItemHolder>().StartHolding(DataIndex, Sprite);
+        Inventory.Instance.RemoveItem(DataIndex);
     }
 }
