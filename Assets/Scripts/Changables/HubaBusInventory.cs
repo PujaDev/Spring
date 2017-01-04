3b6f99e75a2307ce37c84648513829c965ff080d
Ursula Zakovska <u.zakovska@gmail.com>
2017-01-04 02:00:17 +0100

    Add small fixs

---
+++ Assets/Scripts/Changables/HubaBusInventory.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class HubaBusInventory : Inventory
+{
+    public enum HubaBusItemIds
+    {
+        GoldCoins = 0,
+        SilverCoin = 1
+    }
+
+    protected override Inventory GetInstance()
+    {
+        return this;
+    }
+
+    public override void OnStateChanged(GameState newState, GameState oldState)
+    {
+        #region AddItems
+
+        // See what's been added from the last time
+        HashSet<int> addDiff = null;
+        if (oldState == null)
+        {
+            addDiff = newState.HubaBus.PickedUpItems;
+        }
+        else if (!newState.HubaBus.PickedUpItems.SetEquals(oldState.HubaBus.PickedUpItems))
+        {
+            addDiff = new HashSet<int>(newState.HubaBus.PickedUpItems);
+            addDiff.ExceptWith(oldState.HubaBus.PickedUpItems);
+        }
+
+        // If there are differences
+        if (addDiff != null)
+        {
+            // Add new items
+            foreach (var item in addDiff)
+            {
+                AddItem(item);
+            }
+        }
+
+        #endregion
+
+        // Then check states that remove items
+        #region RemoveItems
+
+        // See what's been used from the last time
+        HashSet<int> removeDiff = null;
+        if (oldState == null)
+        {
+            removeDiff = newState.HubaBus.UsedItems;
+        }
+        else
+        {
+            removeDiff = new HashSet<int>(newState.HubaBus.UsedItems);
+            removeDiff.ExceptWith(oldState.HubaBus.UsedItems);
+        }
+
+        // If there are any differences
+        if (removeDiff != null)
+        {
+            // Remove used items
+            foreach (var item in removeDiff)
+            {
+                RemoveItem(item);
+            }
+        }
+
+        #endregion
+
+    }
+}
--- Assets/Scripts/StateManagement/HubaBusSceneState.cs
+++ Assets/Scripts/StateManagement/HubaBusSceneState.cs
@@ -7,17 +7,35 @@ public class HubaBusSceneState : SceneState
 
     public int AngerLevel { get; private set; }
     public Vector3S CharPosition { get; private set; }
+    /// <summary>
+    /// Bus is waiting at the busstop
+    /// </summary>
+    public bool isBusWaiting { get; private set; }
+    /// <summary>
+    /// Bus already arrived and left
+    /// </summary>
+    public bool hasBusLeft { get; private set; }
+    public HashSet<int> PickedUpItems { get; private set; }
+    public HashSet<int> UsedItems { get; private set; }
 
     public HubaBusSceneState()
     {
         AngerLevel = 13;
         CharPosition = new Vector3S(-11.5f, -1.4f, 0f);
+        isBusWaiting = false;
+        hasBusLeft = false;
+        PickedUpItems = new HashSet<int>() { (int)HubaBusInventory.HubaBusItemIds.GoldCoins, (int)HubaBusInventory.HubaBusItemIds.SilverCoin };
+        UsedItems = new HashSet<int>();
     }
 
     private HubaBusSceneState(HubaBusSceneState template)
     {
         AngerLevel = template.AngerLevel;
         CharPosition = template.CharPosition;
+        isBusWaiting = template.isBusWaiting;
+        hasBusLeft = template.hasBusLeft;
+        PickedUpItems = template.PickedUpItems;
+        UsedItems = template.UsedItems;
     }
 
     public HubaBusSceneState SetAngerLevel(int value)
@@ -34,6 +52,34 @@ public class HubaBusSceneState : SceneState
         return copy;
     }
 
+    public HubaBusSceneState SetisBusWaiting(bool value)
+    {
+        var copy = new HubaBusSceneState(this);
+        copy.isBusWaiting = value;
+        return copy;
+    }
+
+    public HubaBusSceneState SethasBusLeft(bool value)
+    {
+        var copy = new HubaBusSceneState(this);
+        copy.hasBusLeft = value;
+        return copy;
+    }
+
+    public HubaBusSceneState SetPickedUpItems(HashSet<int> value)
+    {
+        var copy = new HubaBusSceneState(this);
+        copy.PickedUpItems = value;
+        return copy;
+    }
+
+    public HubaBusSceneState SetUsedItems(HashSet<int> value)
+    {
+        var copy = new HubaBusSceneState(this);
+        copy.UsedItems = value;
+        return copy;
+    }
+
 
     public List<string> CompareChanges(HubaBusSceneState other)
     {
@@ -44,6 +90,18 @@ public class HubaBusSceneState : SceneState
 
         if (!CharPosition.Equals(other.CharPosition))
             result.Add(String.Format("CharPosition:\t{0}\t>>>\t{1}", other.CharPosition, CharPosition));
+
+        if (!isBusWaiting.Equals(other.isBusWaiting))
+            result.Add(String.Format("isBusWaiting:\t{0}\t>>>\t{1}", other.isBusWaiting, isBusWaiting));
+
+        if (!hasBusLeft.Equals(other.hasBusLeft))
+            result.Add(String.Format("hasBusLeft:\t{0}\t>>>\t{1}", other.hasBusLeft, hasBusLeft));
+
+        if (!PickedUpItems.Equals(other.PickedUpItems))
+            result.Add(String.Format("PickedUpItems:\t{0}\t>>>\t{1}", other.PickedUpItems, PickedUpItems));
+
+        if (!UsedItems.Equals(other.UsedItems))
+            result.Add(String.Format("UsedItems:\t{0}\t>>>\t{1}", other.UsedItems, UsedItems));
 
         return result;
     }
