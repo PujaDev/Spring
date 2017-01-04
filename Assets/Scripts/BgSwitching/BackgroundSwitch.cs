05684070e5978e421eac96d34feb1845582bc3ce
Pavel Smejkal <smejkal.pa@gmail.com>
2017-01-04 20:39:12 +0100

    Add triggers to switch layers of trees in forest

---
+++ Assets/Scripts/BgSwitching/BackgroundSwitch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSwitch : MonoBehaviour
+{
+    public SpriteRenderer ObjToSwitch;
+
+    bool IsForeground;
+    Collider2D MyCollider;
+
+    void Awake()
+    {
+        MyCollider = gameObject.GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger enter");
+        IsForeground = !IsForeground;
+        SwitchLayer();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Trigger exit");
+        bool behind = other.bounds.center.y > MyCollider.bounds.center.y;
+
+        if ((IsForeground && !behind)
+            || (!IsForeground && behind))
+        {
+            IsForeground = !IsForeground;
+            SwitchLayer();
+        }
+    }
+
+    private void SwitchLayer()
+    {
+        if (IsForeground)
+            ObjToSwitch.sortingLayerName = "Foreground";
+        else
+            ObjToSwitch.sortingLayerName = "Background";
+    }
+}
