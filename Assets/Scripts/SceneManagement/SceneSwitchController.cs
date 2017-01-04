43f9a0a9221b7ff92b441cb4e5ff97ddcf750bbe
Ursula Zakovska <u.zakovska@gmail.com>
2017-01-04 21:08:28 +0100

    Fix SceneSwitch

--- Assets/Scripts/SceneManagement/SceneSwitchController.cs
+++ Assets/Scripts/SceneManagement/SceneSwitchController.cs
@@ -6,35 +6,24 @@ using Fungus;
 public class SceneSwitchControler : IChangable
 {
     public static SceneSwitchControler Instance { get; private set; }
-
-<<<<<<< HEAD
+    
     protected Flowchart Flowchart;
-
-    public virtual void OnStateChanged(GameState newState, GameState oldState)
-    {
-    }
-
-    protected virtual void Start()
-=======
+    
     override public void OnStateChanged(GameState newState, GameState oldState)
     {
     }
 
     override protected void Start()
->>>>>>> feature/Animations
     {
         if (Instance == null)
         {
             Instance = this;
-<<<<<<< HEAD
 
             Flowchart = GameObject.FindGameObjectWithTag("Scenarios").GetComponent<Flowchart>();
 
             GameState gameState = StateManager.Instance.Subscribe(this);
             OnStateChanged(gameState, null);
-=======
             base.Start();
->>>>>>> feature/Animations
         }
         else
         {
