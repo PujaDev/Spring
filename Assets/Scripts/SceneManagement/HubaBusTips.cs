f0b0f6927abb8c5f0f050efec30045db48e60a5d
Ursula Zakovska <u.zakovska@gmail.com>
2017-01-08 21:34:24 +0100

    Add tips to Huba Bus

---
+++ Assets/Scripts/SceneManagement/HubaBusTips.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HubaBusTips : TipManager
+{
+    protected override string GetTipText(GameState gameState)
+    {
+        var state = gameState.HubaBus;
+
+        if (!state.isDrunk)
+            return "You need to drink your ordered elixir. Let's hope it comes and that it's the right one.";
+        
+
+        return "There's nothing else you need to do";
+    }
+}
