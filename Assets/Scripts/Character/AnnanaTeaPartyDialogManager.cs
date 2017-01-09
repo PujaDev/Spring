eda8df4d7a06e14154fbd2ece1bd75e1230d0382
lelicj <jozef@salsitasoft.com>
2017-01-09 08:18:14 +0100

    Add bubbles

---
+++ Assets/Scripts/Character/AnnanaTeaPartyDialogManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnnanaTeaPartyDialogManager : DialogManager
+{
+    public GameObject BubbleObject;
+    BubbleManager bubble;
+
+    public void Awake()
+    {
+        base.Awake();
+        bubble = BubbleObject.GetComponent<BubbleManager>();
+    }
+
+    public enum DialogueTypes
+    {
+        Poisonous = 0,
+        Edible = 1,
+        Swearing = 2
+    }
+
+    override public void Next()
+    {
+        if (currentLine > 0)
+            return;
+        switch (currentDialogue)
+        {
+            case 0: // thirsty for tea
+                bubble.image_names = new string[] { "Bubble__thirst", "Bubble__tea" };                        
+                bubble.PlayImages(3.2f, BubbleManager.BubbleType.THINK);
+                break;
+
+            case 1: // tea was good - happy
+                bubble.image_names = new string[] { "Bubble__Happpy"};
+                bubble.PlayImages(3.2f, BubbleManager.BubbleType.THINK);
+                break;
+
+            case 2: // tea sucked ass - disgusted
+                bubble.image_names = new string[] { "Bubble__disgusted" };
+                bubble.PlayImages(3.2f, BubbleManager.BubbleType.THINK);
+                break;
+        }
+        currentLine++;
+    }
+}
