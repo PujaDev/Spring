5ed9bb4918fbda7016c37feb1a994bfad176cca9
Pavel Smejkal <smejkal.pa@gmail.com>
2017-01-09 23:10:50 +0100

    Add animation when item is added to inventory

---
+++ Assets/Scripts/UI/InventoryAnimator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryAnimator : MonoBehaviour
+{
+    public static InventoryAnimator Instance { get; private set; }
+
+    public AnimationCurve AddItemCurve;
+    public GameObject ToAnimate;
+    public float Duration;
+
+    private Coroutine AddCr;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Animate()
+    {
+        if (AddCr != null)
+            StopCoroutine(AddCr);
+        StartCoroutine(AddItemCoroutine());
+    }
+
+    private IEnumerator AddItemCoroutine()
+    {
+        Vector3 origScale = ToAnimate.transform.localScale;
+        float time = Duration;
+        while (time > 0)
+        {
+            float normTime = time / Duration;
+            time -= Time.deltaTime;
+
+            ToAnimate.transform.localScale = AddItemCurve.Evaluate(normTime) * origScale;
+
+            yield return null;
+        }
+
+        ToAnimate.transform.localScale = origScale;
+    }
+}
