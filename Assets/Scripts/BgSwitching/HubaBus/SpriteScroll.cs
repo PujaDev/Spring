0c7afa72e49aa1bb4006dc5ed5144698f98d5983
Ursula Zakovska <u.zakovska@gmail.com>
2017-01-11 22:57:08 +0100

    Change Scene 2 BG for bus drive from texture to sprites

---
+++ Assets/Scripts/BgSwitching/HubaBus/SpriteScroll.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteScroll : MonoBehaviour {
+    public float speedX;
+    public float initialOffsetX = 0f;
+    public bool firstOn = true;
+
+    public float SpeedY = 3f;
+    public float Amplitude = 1f;
+    public AnimationCurve curve;
+
+    private float width;
+    private float height;
+    private Vector3 initialPosition;
+
+    void Start () {
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        width -= 0.1f;
+        initialPosition = transform.position + new Vector3(width, 0, 0);
+        if (firstOn)
+            initialOffsetX += width;
+        width = width * 2;
+    }
+
+    void Update()
+    {
+        float offset = (initialOffsetX + Time.time * speedX) % width;
+        height = curve.Evaluate((Time.time % SpeedY) / SpeedY) * Amplitude;
+        transform.position = initialPosition - new Vector3(offset, height, 0);
+    }
+}
---
+++ Assets/Scripts/BgSwitching/HubaBus/TurnRoadTripOn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TurnRoadTripOn : IChangable {
+
+    public override void OnStateChanged(GameState newState, GameState oldState)
+    {
+        if (newState.HubaBus.isInTheBus)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
