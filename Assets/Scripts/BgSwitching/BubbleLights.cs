310c4fbba0e608dbda50f34993fd651ad57bcbe1
Ursula Zakovska <u.zakovska@gmail.com>
2017-01-12 20:58:29 +0100

    Add lights to scene 1 and 3

---
+++ Assets/Scripts/BgSwitching/BubbleLights.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleLights : MonoBehaviour {
+    public Color initialColor;
+    public float Speed;
+    public float diff = 0.1f;
+
+    private float i;
+    private Color startColor;
+    private Color endColor;
+
+    // Use this for initialization
+    void Start () {
+        initialColor = GetComponent<SpriteRenderer>().color;
+        startColor = new Color(Mathf.Clamp(initialColor.r + Random.Range(-diff, diff), 0, 1), Mathf.Clamp(initialColor.g + Random.Range(-diff, diff), 0, 1), Mathf.Clamp(initialColor.b + Random.Range(-diff, diff), 0, 1));
+        endColor = new Color(Mathf.Clamp(initialColor.r + Random.Range(-diff, diff), 0, 1), Mathf.Clamp(initialColor.g + Random.Range(-diff, diff), 0, 1), Mathf.Clamp(initialColor.b + Random.Range(-diff, diff), 0, 1));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Blend towards the current target colour
+        i += Time.deltaTime * Speed;
+        GetComponent<SpriteRenderer>().color = Color.Lerp(startColor, endColor, i);
+
+        // If we've got to the current target colour, choose a new one
+        if (i >= 1)
+        {
+            i = 0;
+            startColor = GetComponent<SpriteRenderer>().color;
+            endColor = new Color(Mathf.Clamp(initialColor.r + Random.Range(-diff, diff), 0, 1), Mathf.Clamp(initialColor.g + Random.Range(-diff, diff), 0, 1), Mathf.Clamp(initialColor.b + Random.Range(-diff, diff), 0, 1));
+        }
+    }
+}
---
+++ Assets/Scripts/BgSwitching/OneBubbleLight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneBubbleLight : MonoBehaviour
+{
+    public Color initialColor;
+    public Color blinkColor;
+    public float Speed;
+
+    private float i;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialColor = new Color(1, 1, 1, 1);
+        blinkColor = new Color(1, 1, 1, 0.7f);
+        Speed = Random.Range(0.5f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Blend towards the current target colour
+        i += Time.deltaTime * Speed;
+        GetComponent<SpriteRenderer>().color = Color.Lerp(initialColor, blinkColor, i);
+
+        // If we've got to the current target colour, choose a new one
+        if (i >= 1)
+        {
+            i = 0;
+            blinkColor = initialColor;
+            initialColor = GetComponent<SpriteRenderer>().color;
+            Speed = Random.Range(0.5f, 2f);
+        }
+    }
+}
